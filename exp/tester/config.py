fe91d3fd8fe21d195bb557b7b9bff264010068a9
yba <yba@umich.edu>
2016-11-11 22:33:40 -0500

    ASR-MT: Add ASR data

--- exp/tester/config.py
+++ exp/tester/config.py
@@ -2,6 +2,6 @@
 ###########################################
 APP_TYPE = 'asr'
 FILE_PATH = '../asr_qa/asr_tedlium_result.txt'
-DIR_PATH = '../asr_sa/speech'
+DIR_PATH = '../asr_mt/speech'
 FIELD_ID = 1 # 0 or 1, indicating which column is the query to be sent
 ###########################################
